ec96c2de90a47e3b06740a35b78a1575f2c3ebf4
4MW23CS018 <ashish.23cs018@sode-edu.in>
2024-09-10 15:26:20 +0530

    J2.java

--- J2.java
+++ J2.java
@@ -12,14 +12,14 @@ public class J2{
     }
     System.out.println(" ");
    
-    for(char i=65;i<=90;i++)
+    for(int i=65;i<=90;i++)
     {
-      System.out.print(" "+i);
+      System.out.print(" "+(char)i);
     }
     System.out.println(" ");
-    for(char i=97;i<=122;i++)
+    for(int i=97;i<=122;i++)
     {
-      System.out.print(" "+i);
+      System.out.print(" "+(char)i);
     }
   }
   
